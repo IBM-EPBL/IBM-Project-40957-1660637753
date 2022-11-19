5da40adf48713ece997fde9921a68ecd66d86bd9
aishu2002-123 <111950201+aishu2002-123@users.noreply.github.com>
2022-11-19 13:47:56 +0530

    Add files via upload

---
+++ Final Deliverable/Mit App/Children_safety.aia (Unzipped Files)/src/appinventor/ai_kiruthikasugi/Children_safety/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"219","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"30","AlignHorizontal":"3","AlignVertical":"2","AppName":"Children_safety","BackgroundImage":"pho","Title":"Screen1","Uuid":"0","$Components":[{"$Name":"Image1","$Type":"Image","$Version":"6","Height":"-1070","Width":"-1050","Picture":"log.jpg","Uuid":"292722884"},{"$Name":"Clock1","$Type":"Clock","$Version":"4","TimerInterval":"2000","Uuid":"462517125"}]}}
+|#
---
+++ Final Deliverable/Mit App/Children_safety.aia (Unzipped Files)/src/appinventor/ai_kiruthikasugi/Children_safety/Screen5.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":[],"YaVersion":"219","Source":"Form","Properties":{"$Name":"Screen5","$Type":"Form","$Version":"30","Uuid":"0","Title":"Screen5","AppName":"Children_safety"}}
+|#
